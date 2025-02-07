0ef79c371ec3b6802391aa55411c1c4ebdb30379
ShashankVenkatesh <shashankvenkatesh7906@gmail.com>
2025-02-07 21:51:53 +0530

    update

---
+++ pages/schemas.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    roles: {
+        type: [String],
+        default: ['user']
+    },
+    profile: {
+        firstName: {
+            type: String
+        },
+        lastName: {
+            type: String
+        },
+        age: {
+            type: Number
+        }
+    },
+    lastLogin: {
+        type: Date
+    }
+}, { timestamps: true });
+
+const users= mongoose.model('user',userSchema);
+module.export = users
